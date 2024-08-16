a56f04c3bec30e74f22ddf33932e732e86c8828a
Sibelle <geisylienaias@gmail.com>
2024-08-16 19:16:22 -0300

    add in github

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/click.mp3')
+
+export const clockTimer = new Audio ('./assets/clock.mp3')
+
+export const alertAudio = new Audio('./assets/alert.mp3')
+
+clockTimer.loop = true
